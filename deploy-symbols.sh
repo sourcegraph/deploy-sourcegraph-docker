a4b88ef47327d6b923e240d264f9073ee0ac8771
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-10-29 01:31:08 -0700

    symbols: remove --expose (now in Dockerfile upstream)
    Fixes #19

--- deploy-symbols.sh
+++ deploy-symbols.sh
@@ -15,7 +15,6 @@ docker run --detach \
     --name=symbols \
     --network=sourcegraph \
     --restart=always \
-    --expose=3184 \
     -e SRC_GIT_SERVERS=gitserver-0:3178 \
     -v ~/sourcegraph-docker/symbols-disk:/mnt/cache \
     sourcegraph/symbols:2.12.0@sha256:c19c072bdebc21f11393a80322a529e043cf3c41a68e37a1dc119e00fde54a7c
