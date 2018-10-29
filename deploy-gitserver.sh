7fcff0ca3cbae710f5f9687f2fba5aa4c319ad44
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-10-29 00:42:38 -0700

    rely on SRC_REPOS_DIR default (fixes #7)

--- deploy-gitserver.sh
+++ deploy-gitserver.sh
@@ -15,7 +15,6 @@ docker run --detach \
     --name=gitserver-0 \
     --network=sourcegraph \
     --restart=always \
-    -e SRC_REPOS_DIR=/data/repos \
     sourcegraph/gitserver:2.12.0@sha256:0fde51ee93b0cdf06ffb0a06ee321f5014b2fe7f9bdc0572dc00bfaaaac4c9b5
 
 echo "Deployed gitserver-0 service"
