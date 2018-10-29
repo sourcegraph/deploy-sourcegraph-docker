b47ea7b26ad4ca93b3096357a4e73288eb434d42
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-10-28 23:40:06 -0700

    simplify redis deployment
    Fixes #14

--- deploy-redis-store.sh
+++ deploy-redis-store.sh
@@ -11,15 +11,10 @@ set -e
 # Ports exposed to other Sourcegraph services: 6379/TCP
 # Ports exposed to the public internet: none
 #
-docker create \
+docker run --detach \
     --name=redis-store \
     --network=sourcegraph \
     -v ~/sourcegraph-docker/redis-store-disk:/redis-data \
-    sourcegraph/redis:18-02-07_8205764_3.2-alpine@sha256:f2957e0973ef16968d4bfacfae5ab08da985257aa7ce358a85152275e3da78e8 \
-    redis-server /etc/redis/redis.conf # command
+    sourcegraph/redis-store:18-10-28_e45f6d82@sha256:1fe101e1f04a8e267fda85c342cd3b974ab4a9e47718b864752f14f1da51579c \
 
-# Create /etc/redis/redis.conf
-docker cp ./redis-store/redis redis-store:/etc/
-
-docker start redis-store
 echo "Deployed redis-store service"
