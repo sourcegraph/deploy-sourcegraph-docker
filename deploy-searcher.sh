764cf7f642a382cb87819d0129e36ccc07fd324f
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-10-29 01:08:27 -0700

    simplify symbols/searcher by relying on new defaults
    Fixes #15

--- deploy-searcher.sh
+++ deploy-searcher.sh
@@ -15,10 +15,7 @@ docker run --detach \
     --name=searcher \
     --network=sourcegraph \
     --restart=always \
-    -e SEARCHER_CACHE_SIZE_MB=100000 \
     -e SRC_GIT_SERVERS=gitserver-0:3178 \
-    -e POD_NAME=searcher \
-    -e CACHE_DIR=/mnt/cache/searcher \
     -v ~/sourcegraph-docker/searcher-disk:/mnt/cache \
     sourcegraph/searcher:2.12.0@sha256:7b7370fb39d8969d46f747f13447cef4a641ed1d21fd5a54583c383ae0462ecc
 
--- deploy-symbols.sh
+++ deploy-symbols.sh
@@ -17,9 +17,6 @@ docker run --detach \
     --restart=always \
     --expose=3184 \
     -e SRC_GIT_SERVERS=gitserver-0:3178 \
-    -e SYMBOLS_CACHE_SIZE_MB=100000 \
-    -e POD_NAME=symbols \
-    -e CACHE_DIR=/mnt/cache/symbols \
     -v ~/sourcegraph-docker/symbols-disk:/mnt/cache \
     sourcegraph/symbols:2.12.0@sha256:c19c072bdebc21f11393a80322a529e043cf3c41a68e37a1dc119e00fde54a7c
 
