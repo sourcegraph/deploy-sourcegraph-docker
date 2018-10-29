9b92dd4712b103e4cb91ab30187bfdb3bda7ffc7
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-10-28 23:33:42 -0700

    remove --privileged (fixed upstream)
    Fixes #1

--- deploy-zoekt-indexserver.sh
+++ deploy-zoekt-indexserver.sh
@@ -14,7 +14,6 @@ set -e
 docker run --detach \
     --name=zoekt-indexserver \
     --network=sourcegraph \
-    --privileged \
     -e SRC_FRONTEND_INTERNAL=sourcegraph-frontend-internal:3090 \
     sourcegraph/zoekt:18-09-14_2f4b0f2@sha256:c51363fc9c8ad8fee2909593bb37475afb04f5ddb5fa67a580b260ab7abcd377 \
     zoekt-sourcegraph-indexserver -index /data/index -sourcegraph_url http://sourcegraph-frontend-internal:3090 -listen :6072 -interval 1m -cpu_fraction "1.0" # command
