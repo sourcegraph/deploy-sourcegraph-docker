6beacadc643ae61c958a2ad412556cdf95e5599f
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2018-10-30 17:38:17 -0700

    unify scaling naming (fixes #12)

--- deploy-gitserver.sh
+++ deploy-gitserver.sh
@@ -17,4 +17,4 @@ docker run --detach \
     --restart=always \
     sourcegraph/gitserver:2.12.0@sha256:0fde51ee93b0cdf06ffb0a06ee321f5014b2fe7f9bdc0572dc00bfaaaac4c9b5
 
-echo "Deployed gitserver-0 service"
+echo "Deployed gitserver service"
