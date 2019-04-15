77587ed053961161443109e17c97224638ffe99f
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-04-15 16:54:33 -0700

    customer replica

--- replicas.sh
+++ replicas.sh
@@ -1,7 +1,7 @@
 # Here you can specify the number of service replicas to deploy.
-NUM_GITSERVER=1
-NUM_SEARCHER=1
-NUM_SYMBOLS=1
+NUM_GITSERVER=18
+NUM_SEARCHER=16
+NUM_SYMBOLS=4
 NUM_FRONTEND=3
 
 addresses() { for i in $(seq 0 $(($2 - 1))); do echo -n "$1$i$3 "; done }
