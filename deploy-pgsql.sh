6e9b65308176fb5fafe193117d34de802bc523a7
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-05-02 16:06:51 -0700

    postgres: bump to 11.1 and clarify that anything 9.6+ is supported

--- deploy-pgsql.sh
+++ deploy-pgsql.sh
@@ -16,6 +16,9 @@ docker run --detach \
     --cpus=4 \
     --memory=2g \
     -v ~/sourcegraph-docker/pgsql-disk:/data/pgdata \
-    sourcegraph/postgres:18-11-26_9.4_a9463ecc@sha256:ca9964a200beb9756704279f82f13caea39eaee8ee9e9e13eba57f2de4952f71
+    sourcegraph/postgres-11.1:19-02-13_22d74790@sha256:10c2ff7a4da422cd75e022b51bef3a0c935f4b3ded335d9679a4f1202db605d2
+
+# Sourcegraph requires PostgreSQL 9.6+. Generally newer versions are better,
+# but anything 9.6 and higher is supported.
 
 echo "Deployed pgsql service"
