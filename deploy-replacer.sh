a9c5cc39bd698b4440fae8a33510356cb221f1c9
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-08-19 19:30:38 -0700

    add replacer deployment

---
+++ deploy-replacer.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+source ./replicas.sh
+
+# Description: Backend for replace operations.
+#
+# Disk: 128GB / non-persistent SSD
+# Network: 100mbps
+# Liveness probe: GET http://replacer:3185/healthz
+# Ports exposed to other Sourcegraph services: 3185/TCP 6060/TCP
+# Ports exposed to the public internet: none
+#
+docker run --detach \
+    --name=replacer \
+    --network=sourcegraph \
+    --restart=always \
+    --cpus=1 \
+    --memory=512m \
+    -e GOMAXPROCS=1 \
+    -v ~/sourcegraph-docker/replacer-disk:/mnt/cache \
+    sourcegraph/replacer:3.7.0
+
+echo "Deployed replacer service"
--- deploy.sh
+++ deploy.sh
@@ -14,13 +14,14 @@ for i in $(seq 0 $(($NUM_GITSERVER - 1))); do (./deploy-gitserver.sh $i &); done
 ./deploy-jaeger-collector.sh
 ./deploy-jaeger-query.sh
 ./init-jaeger-cassandra-schema.sh
-./deploy-lsif-server.sh
+./deploy-lsif-server.sh &
 ./deploy-management-console.sh &
 ./deploy-pgsql.sh &
 ./deploy-prometheus.sh
 ./deploy-query-runner.sh &
 ./deploy-redis-cache.sh &
 ./deploy-redis-store.sh &
+./deploy-replacer.sh &
 ./deploy-repo-updater.sh &
 for i in $(seq 0 $(($NUM_SEARCHER - 1))); do (./deploy-searcher.sh $i &); done
 for i in $(seq 0 $(($NUM_SYMBOLS - 1))); do (./deploy-symbols.sh $i &); done
