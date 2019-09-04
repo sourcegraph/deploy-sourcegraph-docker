f707df3ba97ae0b7db109e18bcdbef0ed401c91a
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2019-09-03 17:14:08 -0700

    use custom prom/grafana images with shared provisioning

--- deploy-grafana.sh
+++ deploy-grafana.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-set -e
+
+set -euf -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Description: Dashboards and graphs for Prometheus metrics.
 #
@@ -17,14 +20,14 @@ docker run --detach \
     --memory=1g \
     -p 0.0.0.0:3000:3000 \
     -v ~/sourcegraph-docker/grafana-disk:/var/lib/grafana \
-    -v $(pwd)/grafana:/etc/grafana \
+    -v ${DIR}/grafana:/sg_config_grafana/provisioning/datasources \
     -e GF_AUTH_ANONYMOUS_ENABLED=true \
     -e GF_AUTH_ANONYMOUS_ORG_NAME=Sourcegraph \
     -e GF_AUTH_ANONYMOUS_ORG_ROLE=Editor \
     -e GF_USERS_ALLOW_SIGN_UP='false' \
     -e GF_USERS_AUTO_ASSIGN_ORG='true' \
     -e GF_USERS_AUTO_ASSIGN_ORG_ROLE=Editor \
-    grafana/grafana:6.1.1@sha256:e7a513bf7f33ef9681b2d35a799136e1ce9330f9055f75dfa2101d812946184b
+    sourcegraph/grafana:3.8
 
 # Add the following lines above if you wish to use an auth proxy with Grafana:
 #
--- deploy-prometheus.sh
+++ deploy-prometheus.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-set -e
+
+set -euf -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 
 # Description: Prometheus collects metrics and aggregates them into graphs.
 #
@@ -16,10 +20,6 @@ docker run --detach \
     --cpus=4 \
     --memory=8g \
     -p 0.0.0.0:9090:9090 \
-    -v ~/sourcegraph-docker/prometheus-disk:/prometheus-data \
-    -v $(pwd)/prometheus:/etc/prometheus \
-    sourcegraph/prometheus:v1.4.1@sha256:1a02878ac9a0f532a68673bda93d406b21786706446c42a40cb3ed3e867278c0 \
-    -config.file=/etc/prometheus/prometheus.yml \
-    -storage.local.path=/prometheus-data \
-    -web.console.libraries=/etc/prometheus/console_libraries \
-    -web.console.templates=/etc/prometheus/consoles
+    -v ~/sourcegraph-docker/prometheus-disk:/prometheus \
+    -v ${DIR}/prometheus:/sg_prometheus_add_ons \
+    sourcegraph/prometheus:3.8
