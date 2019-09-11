22a06479da2ff16bce798645642957eac089e468
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-09-11 16:11:41 -0700

    prometheus: use new disk folder to make upgrading seamless

--- deploy-prometheus.sh
+++ deploy-prometheus.sh
@@ -17,6 +17,6 @@ docker run --detach \
     --cpus=4 \
     --memory=8g \
     -p 0.0.0.0:9090:9090 \
-    -v ~/sourcegraph-docker/prometheus-disk:/prometheus \
+    -v ~/sourcegraph-docker/prometheus-v2-disk:/prometheus \
     -v $(pwd)/prometheus:/sg_prometheus_add_ons \
     sourcegraph/prometheus:v2.12.0@sha256:aca8cf12d41cbf5d8885ed675fa017b83ad73af96d86bbe5bcead45ca8e958f3
