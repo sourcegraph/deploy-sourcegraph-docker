1d40c08ac3c96dee931217d9bafc8f5da728009c
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-09-15 21:35:25 -0700

    add default REPLACER_URL

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -25,6 +25,7 @@ docker run --detach \
     -e SYMBOLS_URL="$(addresses "http://symbols-" $NUM_SYMBOLS ":3184")" \
     -e SRC_FRONTEND_INTERNAL=sourcegraph-frontend-internal:3090 \
     -e REPO_UPDATER_URL=http://repo-updater:3182 \
+    -e REPLACER_URL=http://replacer:3185 \
     -e ZOEKT_HOST=zoekt-webserver:6070 \
     -e LSIF_SERVER_URL=http://lsif-server:3186 \
     -e GRAFANA_SERVER_URL=htto://grafana:3000 \
