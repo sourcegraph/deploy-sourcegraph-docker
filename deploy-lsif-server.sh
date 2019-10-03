db9f6e0ea2863d9e38d51a18272a1c5a7a54866c
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-10-03 00:07:21 -0700

    Release v3.8.0

--- deploy-lsif-server.sh
+++ deploy-lsif-server.sh
@@ -19,6 +19,6 @@ docker run --detach \
     -e GOMAXPROCS=2 \
     -e LSIF_STORAGE_ROOT=/lsif-storage \
     -v ~/sourcegraph-docker/lsif-server-disk:/lsif-storage \
-    sourcegraph/lsif-server:3.7.2
+    sourcegraph/lsif-server:3.8.0
 
 echo "Deployed lsif-server service"
