68492f511a78f6c203d33bab237d27e8662a5853
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-10-04 13:44:48 -0700

    prometheus: scrape metrics from syntect-server

--- deploy-syntect-server.sh
+++ deploy-syntect-server.sh
@@ -6,7 +6,7 @@ set -e
 # Disk: none
 # Network: 100mbps
 # Liveness probe: HTTP GET http://syntect-server:9238/health
-# Ports exposed to other Sourcegraph services: 9238/TCP
+# Ports exposed to other Sourcegraph services: 9238/TCP 6060/TCP
 # Ports exposed to the public internet: none
 #
 docker run --detach \
