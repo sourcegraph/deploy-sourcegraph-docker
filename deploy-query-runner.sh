133bbdd1afa01053148d07744c38278934f3ff66
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-10-04 02:08:06 -0700

    update to 3.8.2 images

--- deploy-query-runner.sh
+++ deploy-query-runner.sh
@@ -18,6 +18,6 @@ docker run --detach \
     -e GOMAXPROCS=1 \
     -e SRC_FRONTEND_INTERNAL=sourcegraph-frontend-internal:3090 \
     -e JAEGER_AGENT_HOST='jaeger-agent' \
-    sourcegraph/query-runner:3.8.1
+    sourcegraph/query-runner:3.8.2
 
 echo "Deployed query-runner service"
