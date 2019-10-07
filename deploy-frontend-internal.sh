fcd8d0347e9ffe8ca0d808a00c9fdbd356b39747
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-10-07 12:39:07 -0700

    reduce diff for shipping (avoids any confusion/typos)

--- deploy-frontend-internal.sh
+++ deploy-frontend-internal.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 source ./replicas.sh
-source ./a8n.sh
 
 # Description: Serves the internal Sourcegraph frontend API.
 #
@@ -30,6 +29,6 @@ docker run --detach \
     -e LSIF_SERVER_URL=http://lsif-server:3186 \
     -e GRAFANA_SERVER_URL=http://grafana:3000 \
     -v ~/sourcegraph-docker/sourcegraph-frontend-internal-0-disk:/mnt/cache \
-    sourcegraph/frontend@$FRONTEND_IMAGE_HASH
+    sourcegraph/frontend@sha256:a658bbe4cbb40ba16891d036bc2892c80c98a05c5c4184d336020e7ff6fab5cd
 
 echo "Deployed sourcegraph-frontend-internal service"
