f7d5bc99059b30dae7f54adc440f34a574941441
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-01-28 15:03:44 -0800

    Update deploy-syntect-server.sh

--- deploy-syntect-server.sh
+++ deploy-syntect-server.sh
@@ -16,4 +16,5 @@ docker run --detach \
     --cpus=4 \
     --memory=6g \
     index.docker.io/sourcegraph/syntect_server:2b5a3fb@sha256:ef5529cafdc68d5a21edea472ee8ad966878b173044aa5c3db93bc3d84765b1f
+
 echo "Deployed syntect-server service"
