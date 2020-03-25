f4fb645aeb55cb3218f82aec5c899ce295b350bb
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2020-03-25 13:02:29 -0700

    v3.14.0-1 (#91)
    * lsif-server: correct file permissions on new deployments
    
    See https://github.com/sourcegraph/sourcegraph/pull/9321
    
    * cadvisor: add support for Amazon Linux (non-v2)
    
    This volume mount is required for cadvisor to work correctly on Amazon Linux (non-v2)
    host OSs, and likely other older Linux distros.
    
    I have also tested on Ubuntu Xenial, and with a volume mount that does not exist,
    and neither appear to cause any issues when `/cgroup` does not exist on the host OS
    so this should be fine to add for all deployments.

--- deploy-lsif-server.sh
+++ deploy-lsif-server.sh
@@ -20,6 +20,6 @@ docker run --detach \
     -e LSIF_STORAGE_ROOT=/lsif-storage \
     -e SRC_FRONTEND_INTERNAL=sourcegraph-frontend-internal:3090 \
     -v ~/sourcegraph-docker/lsif-server-disk:/lsif-storage \
-    index.docker.io/sourcegraph/lsif-server:3.14.0@sha256:75bdc5100e95f9b113fbfd4c40efb2b88f38ce62b07be019c8dac29517db97dc
+    index.docker.io/sourcegraph/lsif-server:3.14.0-1@sha256:ed683d2579d807458a892eab2f88fa5618b82a1ae7af02d5e7de1af3f6eea4a7
 
 echo "Deployed lsif-server service"
