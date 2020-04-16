cdc9604e5d2d757e8f66cdd61ada2668479c77be
Beyang Liu <beyang@sourcegraph.com>
2020-04-15 17:17:43 -0700

    jaeger: set --memory.max-traces

--- deploy-jaeger.sh
+++ deploy-jaeger.sh
@@ -20,4 +20,5 @@ docker run --detach \
     -p 0.0.0.0:5778:5778 \
     -p 0.0.0.0:6831:6831 \
     -p 0.0.0.0:6832:6832 \
-    index.docker.io/sourcegraph/jaeger-all-in-one:1.17.1@sha256:46bfa2ac08dd08181ab443ef966d664048a6c6ac725054bcc1fbfda5bd4040a3
+    index.docker.io/sourcegraph/jaeger-all-in-one:1.17.1@sha256:46bfa2ac08dd08181ab443ef966d664048a6c6ac725054bcc1fbfda5bd4040a3 \
+    --memory.max-traces=20000
