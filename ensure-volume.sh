bd23a1c8f13e5a6b6efd88da432bd1aea633a749
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2020-05-07 20:47:14 -0700

    ensure-volume: fix syntax error

--- ensure-volume.sh
+++ ensure-volume.sh
@@ -4,7 +4,7 @@ set -euf -o pipefail
 VOLUME=$1
 USER_ID=$2
 
-if [ ! -d $VOLUME ]; then
-    # explicitly not used in customer-replica branch, since customer does not run this today.
-    #mkdir -p $VOLUME && sudo chown $USER_ID:$USER_ID $VOLUME
-fi
+# explicitly not used in customer-replica branch, since customer does not run this today.
+#if [ ! -d $VOLUME ]; then
+#    mkdir -p $VOLUME && sudo chown $USER_ID:$USER_ID $VOLUME
+#fi
