eab7b1ebb2ece4297a8ee853d862216ae3bb7c33
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2020-05-07 20:30:58 -0700

    no ensuring volumes

--- ensure-volume.sh
+++ ensure-volume.sh
@@ -5,5 +5,6 @@ VOLUME=$1
 USER_ID=$2
 
 if [ ! -d $VOLUME ]; then
-    mkdir -p $VOLUME && sudo chown $USER_ID:$USER_ID $VOLUME
+    # explicitly not used in customer-replica branch, since customer does not run this today.
+    #mkdir -p $VOLUME && sudo chown $USER_ID:$USER_ID $VOLUME
 fi
