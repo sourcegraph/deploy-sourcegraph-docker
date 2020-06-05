ddaaa1a61a24247cb95fe22cfc04c93e01f2a616
Robert Lin <robert@bobheadxi.dev>
2020-06-04 18:46:16 -0700

    cadvisor: apply args to reduce output

--- deploy-cadvisor.sh
+++ deploy-cadvisor.sh
@@ -10,6 +10,7 @@ source ./replicas.sh
 # Ports exposed to other Sourcegraph services: 8080/TCP
 # Ports exposed to the public internet: none
 #
+# See docker-compose.yml for docs on the cAdvisor args used
 sudo docker run --detach \
     --name=cadvisor \
     --network=sourcegraph \
@@ -21,6 +22,11 @@ sudo docker run --detach \
     --volume=/sys:/sys:ro \
     --volume=/var/lib/docker/:/var/lib/docker:ro \
     --volume=/dev/disk/:/dev/disk:ro \
-    index.docker.io/sourcegraph/cadvisor:insiders@sha256:4074c8bc608b78af3ca3d6e60b3794369a190ab2efd992e31b3079b075401efa
+    index.docker.io/sourcegraph/cadvisor:insiders@sha256:4074c8bc608b78af3ca3d6e60b3794369a190ab2efd992e31b3079b075401efa \
+    --disable_metrics=percpu,sched,tcp,udp \
+    --housekeeping_interval=10s \
+    --max_housekeeping_interval=15s \
+    --event_storage_event_limit=default=0 \
+    --event_storage_age_limit=default=0
 
 echo "Deployed cadvisor"
