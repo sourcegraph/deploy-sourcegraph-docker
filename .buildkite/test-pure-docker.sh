aa6873e65d89a838b6493985c4f140d5b75c804b
Dave Try <dave@sourcegraph.com>
2020-08-18 07:31:00 -0500

    more testing

--- .buildkite/test-pure-docker.sh
+++ .buildkite/test-pure-docker.sh
@@ -3,6 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 set -euxo pipefail
 
-/usr/bin/vagrant up
+/usr/bin/vagrant box add ../../sg-buildkite.box --name sg-buildkite
+/usr/bin/vagrant
 # ./smoke-test.sh
 /usr/bin/vagrant destroy -f
