f63b551295a819dbfc054ab8db512b5ebbf3f53c
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2020-08-18 20:20:04 -0700

    pure-docker test fixes (#135)
    * do not deploy in parallel
    
    On Ubuntu 18.04 with the `docker-ce` package, launching 59 `docker run -it` downloads
    concurrently results in some of the `docker run` commands deadlocking forever and the
    containers never starting.
    
    This has not affected https://app.hubspot.com/contacts/2762526/company/407948923/ because
    they bake an AMI with each Docker image saved into it first.
    
    This was one cause of deadlocks in https://github.com/sourcegraph/sourcegraph/issues/12996
    as well as a cause of other issues (teardown.sh running before deploy.sh finished.)
    
    * test/pure-docker: static DNS and more
    
    - Denote sections of work with `echo`.
    - Use static DNS which appears to help in some situations (and at worse, doesn't hurt.)
    - Remove sleep statements no longer needed now that deploy.sh does not
      return before finishing.
    
    * test/pure-docker: correct number of containers on customer-replica branch

--- deploy.sh
+++ deploy.sh
@@ -7,26 +7,26 @@ source ./replicas.sh
 docker network create sourcegraph &> /dev/null || true
 
 ./deploy-cadvisor.sh
-./deploy-github-proxy.sh &
-for i in $(seq 0 $(($NUM_GITSERVER - 1))); do (./deploy-gitserver.sh $i &); done
+./deploy-github-proxy.sh
+for i in $(seq 0 $(($NUM_GITSERVER - 1))); do (./deploy-gitserver.sh $i); done
 ./deploy-grafana.sh
 ./deploy-jaeger.sh
-./deploy-precise-code-intel-bundle-manager.sh &
-./deploy-precise-code-intel-worker.sh &
-./deploy-pgsql.sh &
+./deploy-precise-code-intel-bundle-manager.sh
+./deploy-precise-code-intel-worker.sh
+./deploy-pgsql.sh
 ./deploy-prometheus.sh
-./deploy-query-runner.sh &
-./deploy-redis-cache.sh &
-./deploy-redis-store.sh &
-./deploy-repo-updater.sh &
-for i in $(seq 0 $(($NUM_SEARCHER - 1))); do (./deploy-searcher.sh $i &); done
-for i in $(seq 0 $(($NUM_SYMBOLS - 1))); do (./deploy-symbols.sh $i &); done
-./deploy-syntect-server.sh &
-for i in $(seq 0 $(($NUM_INDEXED_SEARCH - 1))); do (./deploy-zoekt-indexserver.sh $i &); done
-for i in $(seq 0 $(($NUM_INDEXED_SEARCH - 1))); do (./deploy-zoekt-webserver.sh $i &); done
+./deploy-query-runner.sh
+./deploy-redis-cache.sh
+./deploy-redis-store.sh
+./deploy-repo-updater.sh
+for i in $(seq 0 $(($NUM_SEARCHER - 1))); do ./deploy-searcher.sh $i; done
+for i in $(seq 0 $(($NUM_SYMBOLS - 1))); do ./deploy-symbols.sh $i; done
+./deploy-syntect-server.sh
+for i in $(seq 0 $(($NUM_INDEXED_SEARCH - 1))); do ./deploy-zoekt-indexserver.sh $i; done
+for i in $(seq 0 $(($NUM_INDEXED_SEARCH - 1))); do ./deploy-zoekt-webserver.sh $i; done
 
 # Redis must be started before these.
 ./deploy-frontend-internal.sh
-for i in $(seq 0 $(($NUM_FRONTEND - 1))); do (./deploy-frontend.sh $i &); done
+for i in $(seq 0 $(($NUM_FRONTEND - 1))); do ./deploy-frontend.sh $i; done
 ./deploy-caddy.sh
 wait
