bfd12eb167e90ed969b3ad169e8f6d3c750ca095
Dave Try <dave@sourcegraph.com>
2020-08-19 10:20:05 -0500

    specify provider

--- .buildkite/test-pure-docker.sh
+++ .buildkite/test-pure-docker.sh
@@ -4,6 +4,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 set -euxo pipefail
 
 cd test/pure-docker/
-vagrant plugin install vagrant-google
+vagrant plugin install vagrant-google --provider=virtualbox
 vagrant up pure-docker-test-local
 vagrant destroy -f
