a90049075485e3d415b17639c533c60e4fcc5726
Dave Try <dave@sourcegraph.com>
2020-08-20 12:38:12 -0500

    lowering cpu for zoekt

--- deploy-zoekt-webserver.sh
+++ deploy-zoekt-webserver.sh
@@ -17,7 +17,7 @@ docker run --detach \
     --hostname=zoekt-webserver-$1 \
     --network=sourcegraph \
     --restart=always \
-    --cpus=16 \
+    --cpus=4 \
     --memory=100g \
     -e GOMAXPROCS=16 \
     -e HOSTNAME=zoekt-webserver-$1:6070 \
