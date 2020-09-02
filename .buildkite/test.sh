0c8b4919f88f7c9a83cfef507022971fb74cf286
Dave Try <dave@sourcegraph.com>
2020-09-02 15:28:21 -0500

    test script failure

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -4,6 +4,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 set -euxo pipefail
 
 box=$1
+exit_code=0
 
 cd test/
 
@@ -11,5 +12,10 @@ if ! vagrant plugin list --no-tty | grep vagrant-google; then
 	vagrant plugin install vagrant-google
 fi
 
-vagrant up $box --provider=google
+vagrant up $box --provider=google || exit_code=$?
 vagrant destroy -f $box
+
+if [ "$exit_code" != 0 ]
+then
+  exit 1
+fi
