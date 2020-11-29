6ba015d6edaf3329f2a82de0751da38cf42f1c6c
Robert Lin <robert@bobheadxi.dev>
2020-11-29 13:17:49 +0800

    tools: generate update-docker-tags args from upstream

---
+++ tools/enforce-tags/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/sourcegraph/sourcegraph/enterprise/dev/ci/images"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatal("constraint, directory arguments must be provided")
+	}
+	var (
+		constraint = os.Args[1]
+		dir        = os.Args[2]
+	)
+	args := []string{
+		"run",
+		"github.com/slimsag/update-docker-tags",
+	}
+	for _, image := range images.SourcegraphDockerImages {
+		args = append(args, fmt.Sprintf("-enforce=sourcegraph/%s=%s", image, constraint))
+	}
+	args = append(args, dir)
+	log.Println(strings.Join(args, " "))
+
+	cmd := exec.Command("go", args...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		log.Fatal((err))
+	}
+}
