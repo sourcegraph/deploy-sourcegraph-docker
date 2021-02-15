064fd05484a595ba30a0b9443b7752e3258d0b1e
Dave Try <dave@sourcegraph.com>
2021-02-15 09:30:41 -0500

    working locally

--- tools/migrate.sh
+++ tools/migrate.sh
@@ -1,39 +1,53 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -uxo pipefail
 
 # shellcheck source=migration_vars
 source migration_vars
 
 IMAGE=${IMAGE:-sourcegraph/server}
-# trap 'rm -f ./*pg.out' EXIT
+sg_dump_file=$(mktemp)
+codeintel_dump_file=$(mktemp)
 
+trap 'rm -f ${sg_dump_file} ${codeintel_dump_file}' EXIT
+
+# Get the  ID of the sourcegraph container
 CONTAINER_ID=$(ssh "${SRC_HOST}" docker ps | grep "${IMAGE}:${VERSION}" | cut -f 1 -d ' ')
 
-ssh -t "${SRC_HOST}" "docker exec ${CONTAINER_ID} sh -c 'pg_dumpall --verbose --username=postgres' " >sourcegraph_pgall.out
-# ssh -t "${SRC_HOST}" "docker exec ${CONTAINER_ID} sh -c 'pg_dump --verbose --username=postgres sourcegraph-codeintel' " >codeintel_pg.out
+# Dump sourcegraph db and alter script for migration
+ssh ${SRC_HOST} "docker exec ${CONTAINER_ID} pg_dump -C --username=postgres sourcegraph" >${sg_dump_file}
+sed '/^CREATE DATABASE/a CREATE USER postgres WITH SUPERUSER CREATEROLE CREATEDB REPLICATION BYPASSRLS;' ${sg_dump_file}
+printf '\connect -reuse-previous=on dbname=postgres\nDROP DATABASE sg;\nALTER DATABASE "sourcegraph" RENAME TO sg;\nALTER DATABASE sg OWNER TO sg;\n' >> ${sg_dump_file}
 
+# Dump codeintel db and alter script for migration
+ssh ${SRC_HOST} "docker exec ${CONTAINER_ID} pg_dump -C --username=postgres sourcegraph-codeintel" >${codeintel_dump_file}
+sed '/^CREATE DATABASE/a CREATE USER postgres WITH SUPERUSER CREATEROLE CREATEDB REPLICATION BYPASSRLS;' ${codeintel_dump_file}
+printf '\connect -reuse-previous=on dbname=postgres\nDROP DATABASE sg;\nALTER DATABASE "sourcegraph-codeintel" RENAME TO sg;\nALTER DATABASE sg OWNER TO sg;\n' >> ${codeintel_dump_file}
 
-chmod 777 ./*pg.out
-scp ./*pg.out "${DST_HOST}:/tmp/"
+# Upload database dumps to new docker-compose deployment
+chmod 544 ${sg_dump_file} ${codeintel_dump_file}
+scp ${sg_dump_file} "${DST_HOST}:${sg_dump_file}"
+scp ${codeintel_dump_file} "${DST_HOST}:${codeintel_dump_file}"
 
+# Migrate databases and start new deployment
 ssh -t "${DST_HOST}" "
 pushd ${COMPOSE_DIR}
 docker-compose down --volumes
-docker-compose -f pgsql-only-migrate.docker-compose.yaml up -d
-sleep 15
-docker cp /tmp/sourcegraph_pgall.out pgsql:/tmp/
+docker-compose -f db-only-migrate.docker-compose.yaml up -d
+sleep 10
+docker cp ${sg_dump_file} pgsql:${sg_dump_file}
+docker cp ${codeintel_dump_file} codeintel-db:${codeintel_dump_file}
+
+docker exec pgsql sh -c 'psql -v ERROR_ON_STOP=1 -U sg -f ${sg_dump_file} postgres'
+docker exec codeintel-db sh -c 'psql -v ERROR_ON_STOP=1 -U sg -f ${codeintel_dump_file} postgres'
+
+docker-compose -f docker-compose.yaml up -d
+echo 'TEST: Checking frontend is accessible for 1 minute'
+
+for i in {1..6}; do
+curl -f http://localhost:80
+curl -f http://localhost:80/healthz
+sleep 10
+done
 "
 
-# docker exec pgsql sh -c 'psql -v ERROR_ON_STOP=1 -U sg -f /tmp/sourcegraph_pg.out postgres'
-#  docker exec pgsql sh -c 'psql -U sg postgres -c \"DROP DATABASE sg;\"'
-# docker exec pgsql sh -c 'psql -U sg postgres -c \"ALTER DATABASE sourcegraph RENAME TO sg; ALTER DATABASE sg OWNER TO sg;\"'
-# docker cp codeintel_pg.out codeintel-db:/tmp/
-# docker exec codeintel-db sh -c 'psql -U sg -f /tmp/codeintel_pg.out postgres'
-# docker exec codeintel-db sh -c 'psql -U sg postgres -c \"DROP DATABASE sg;\"'
-# docker exec codeintel-db sh -c 'psql -U sg postgres -c \"ALTER DATABASE sourcegraph-codeintel RENAME TO sg; ALTER DATABASE sg OWNER TO sg;\"'
-
-# pushd ${COMPOSE_DIR}
-# docker-compose -f docker-compose.yaml up -d
-# sleep 30
-# curl localhost 2>&1 | grep sign-in
