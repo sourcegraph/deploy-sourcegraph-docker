26f45c1e08d6bf8bb911e6ed159d6513014d3441
Dave Try <davetry@gmail.com>
2021-02-15 10:00:43 -0500

    lowercase vars

--- tools/migrate.sh
+++ tools/migrate.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # shellcheck source=migration_vars
 source migration_vars
 
-IMAGE=${IMAGE:-sourcegraph/server}
+iamge=${image:-sourcegraph/server}
 sg_dump_file=$(mktemp)
 codeintel_dump_file=$(mktemp)
 
@@ -13,29 +13,29 @@ trap 'rm -f ${sg_dump_file} ${codeintel_dump_file}' EXIT
 
 # Get the  ID of the sourcegraph container
 echo "Obtaing sourcegraph container ID"
-CONTAINER_ID=$(ssh "${SRC_HOST}" docker ps | grep "${IMAGE}:${VERSION}" | cut -f 1 -d ' ')
+container_id=$(ssh "${src_host}" docker ps | grep "${image}:${version}" | cut -f 1 -d ' ')
 
 # Dump sourcegraph db and alter script for migration
 echo "Dumping sourcegraph database"
-ssh ${SRC_HOST} "docker exec ${CONTAINER_ID} pg_dump -C --username=postgres sourcegraph" >${sg_dump_file}
-sed -n --silent '/^CREATE DATABASE/a CREATE USER postgres WITH SUPERUSER CREATEROLE CREATEDB REPLICATION BYPASSRLS;' ${sg_dump_file} > /dev/null 2>&1
-printf '\connect -reuse-previous=on dbname=postgres\nDROP DATABASE sg;\nALTER DATABASE "sourcegraph" RENAME TO sg;\nALTER DATABASE sg OWNER TO sg;\n' >> ${sg_dump_file}
+ssh ${src_host} "docker exec ${container_id} pg_dump -C --username=postgres sourcegraph" >${sg_dump_file}
+sed -n --silent '/^CREATE DATABASE/a CREATE USER postgres WITH SUPERUSER CREATEROLE CREATEDB REPLICATION BYPASSRLS;' ${sg_dump_file} >/dev/null 2>&1
+printf '\connect -reuse-previous=on dbname=postgres\nDROP DATABASE sg;\nALTER DATABASE "sourcegraph" RENAME TO sg;\nALTER DATABASE sg OWNER TO sg;\n' >>${sg_dump_file}
 
 # Dump codeintel db and alter script for migration
 echo "Dumping codeintel database"
-ssh ${SRC_HOST} "docker exec ${CONTAINER_ID} pg_dump -C --username=postgres sourcegraph-codeintel" >${codeintel_dump_file}
-sed -n --silent '/^CREATE DATABASE/a CREATE USER postgres WITH SUPERUSER CREATEROLE CREATEDB REPLICATION BYPASSRLS;' ${codeintel_dump_file} > /dev/null 2>&1
-printf '\connect -reuse-previous=on dbname=postgres\nDROP DATABASE sg;\nALTER DATABASE "sourcegraph-codeintel" RENAME TO sg;\nALTER DATABASE sg OWNER TO sg;\n' >> ${codeintel_dump_file}
+ssh ${src_host} "docker exec ${container_id} pg_dump -C --username=postgres sourcegraph-codeintel" >${codeintel_dump_file}
+sed -n --silent '/^CREATE DATABASE/a CREATE USER postgres WITH SUPERUSER CREATEROLE CREATEDB REPLICATION BYPASSRLS;' ${codeintel_dump_file} >/dev/null 2>&1
+printf '\connect -reuse-previous=on dbname=postgres\nDROP DATABASE sg;\nALTER DATABASE "sourcegraph-codeintel" RENAME TO sg;\nALTER DATABASE sg OWNER TO sg;\n' >>${codeintel_dump_file}
 
 # Upload database dumps to new docker-compose deployment
 echo "Uploading dumps to new deployment"
 chmod 544 ${sg_dump_file} ${codeintel_dump_file}
-scp ${sg_dump_file} "${DST_HOST}:${sg_dump_file}"
-scp ${codeintel_dump_file} "${DST_HOST}:${codeintel_dump_file}"
+scp ${sg_dump_file} "${dst_host}:${sg_dump_file}"
+scp ${codeintel_dump_file} "${dst_host}:${codeintel_dump_file}"
 
 # Migrate databases and start new deployment
-ssh -t "${DST_HOST}" "
-pushd ${COMPOSE_DIR}
+ssh -t "${dst_host}" "
+pushd ${compose_dir}
 docker-compose down --volumes
 docker-compose -f db-only-migrate.docker-compose.yaml up -d
 sleep 10
@@ -56,4 +56,3 @@ sleep 10
 done
 echo 'Migration completed'
 "
-
