6d158a002bf69a65169d15fc8a818f51b2333211
Stephen Gutekanst <stephen@sourcegraph.com>
2021-02-19 15:27:24 -0700

    test: add codeinsights-db permissions
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- test/volume-config.sh
+++ test/volume-config.sh
@@ -21,6 +21,6 @@ pushd ~/sourcegraph-docker
 chown -R 100:101 gitserver* prometheus-v2* repo-updater* searcher* sourcegraph-frontend* symbols* zoekt* minio-disk
 chown -R 999:1000 redis-store-disk redis-cache-disk
 chown -R 472:472 grafana-disk
-chown -R 999:999 pgsql-disk codeintel-db-disk
+chown -R 999:999 pgsql-disk codeintel-db-disk codeinsights-db-disk
 popd
 echo "Ready to deploy"
