a0f54fc382607c37bb0a98f5b858001faad308c6
Andre Eleuterio <11207474+andreeleuterio@users.noreply.github.com>
2021-05-07 11:47:30 -0300

    Update sourcegraph/postgres-12.6 images (#390)
    Co-authored-by: Sourcegraph <batch-changes@sourcegraph.com>

--- deploy-pgsql.sh
+++ deploy-pgsql.sh
@@ -19,7 +19,7 @@ docker run --detach \
     --memory=2g \
     -e PGDATA=/var/lib/postgresql/data/pgdata \
     -v $VOLUME:/var/lib/postgresql/data/ \
-    index.docker.io/sourcegraph/postgres-12.6:91468_2021-03-30_b77d2e6@sha256:aa937c1c8ab20f3c809f04480d5a73791b05be59d3183726fd499ae0a123e982
+    index.docker.io/sourcegraph/postgres-12.6:95244_2021-05-06_2c1f77e@sha256:35040317490324a15e1259c9023a726eb27c694530f6f8877e87d337c7b97778
 
 # Sourcegraph requires PostgreSQL 12+. Generally newer versions are better,
 # but anything 12 and higher is supported.
