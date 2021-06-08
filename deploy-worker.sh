2c0a88149b2d2ba638e9d1ef0a082ba1ae8f38b9
Eric Fritz <eric@sourcegraph.com>
2021-06-07 21:20:05 -0500

    Introduce worker service (#440)

---
+++ deploy-worker.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+source ./replicas.sh
+
+# Description: Manages background processes.
+#
+# Disk: 128GB / non-persistent SSD
+# Network: 100mbps
+# Liveness probe: n/a
+# Ports exposed to other Sourcegraph services: 3189/TCP 6060/TCP
+# Ports exposed to the public internet: none
+#
+VOLUME="$HOME/sourcegraph-docker/worker-disk"
+./ensure-volume.sh $VOLUME 100
+docker run --detach \
+    --name=worker \
+    --network=sourcegraph \
+    --restart=always \
+    --cpus=4 \
+    --memory=4g \
+    -e GOMAXPROCS=1 \
+    -e SRC_FRONTEND_INTERNAL=sourcegraph-frontend-internal:3090 \
+    -e JAEGER_AGENT_HOST=jaeger \
+    -v $VOLUME:/mnt/cache \
+    index.docker.io/sourcegraph/worker:insiders@sha256:c724fc8d29c30135d30e41a798b6a7688b5eda0a3b9a7b74753faf7028c02b41
+
+echo "Deployed worker service"
