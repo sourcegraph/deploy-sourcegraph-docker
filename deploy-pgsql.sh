22b394b9cf084084cbe82ca8ded84eef688222e4
Geoffrey Gilmore <geoffrey@sourcegraph.com>
2021-07-21 15:11:35 -0700

    Update deploy-pgsql.sh

--- deploy-pgsql.sh
+++ deploy-pgsql.sh
@@ -19,7 +19,7 @@ docker run --detach \
     --memory=2g \
     -e PGDATA=/var/lib/postgresql/data/pgdata \
     -v $VOLUME:/var/lib/postgresql/data/ \
-    index.docker.io/sourcegraph/postgres-12.6-alpine:3.30.0@sha256:f1f8807733f442c6c62a86e8f952761bad1a413ed32872a756cb92481b7108df
+    index.docker.io/sourcegraph/postgres-12.6-alpine:3.30.0@sha256:439246675646322cf6b22a277ab86d790287bc4c532e2a2f4a4e0538d06cd206
 
 # Sourcegraph requires PostgreSQL 12+. Generally newer versions are better,
 # but anything 12 and higher is supported.
