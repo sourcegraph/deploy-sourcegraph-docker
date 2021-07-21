9763a66e0cfd65637c2772c8a2c99407a8a72f15
Geoffrey Gilmore <geoffrey@sourcegraph.com>
2021-07-21 15:36:14 -0700

    Update ensure-volume.sh

--- ensure-volume.sh
+++ ensure-volume.sh
@@ -4,6 +4,10 @@ set -euf -o pipefail
 VOLUME=$1
 USER_ID=$2
 
+if [[ "${CI:-"false"}" == "true" ]]; then
+  exit 0
+fi
+
 if [ ! -d $VOLUME ]; then
-    mkdir -p $VOLUME && sudo chown $USER_ID:$USER_ID $VOLUME && sudo chmod 777 $VOLUME
+  mkdir -p $VOLUME && sudo chown $USER_ID:$USER_ID $VOLUME && sudo chmod 777 $VOLUME
 fi
