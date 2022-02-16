7b1d40ad023dbc7ccbba4a0aaab9c94f14bf59b9
Crystal Augustus <crystal.augustus@sourcegraph.com>
2022-02-16 18:42:50 -0500

    Add pure-docker migrator support

---
+++ pure-docker/deploy-migrator.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+# Description: Performs database migrations
+#
+# Network: 100mbps
+# Liveness probe: n/a
+# Ports exposed to other Sourcegraph services: none
+# Ports exposed to the public internet: none
+#
+
+docker run --detach \
+    --name=migrator \
+    --network=sourcegraph \
+    --restart=on-failure \
+    --cpus=1 \
+    --memory=1g \
+    -e PGHOST=pgsql \
+    -e PGUSER=sg \
+    -e CODEINTEL_PGUSER=sg \
+    -e CODEINTEL_PGHOST=codeintel-db \
+    -e CODEINSIGHTS_PGDATASOURCE=postgres://postgres:password@codeinsights-db:5432/postgres \
+    index.docker.io/sourcegraph/migrator:insiders@sha256:38f01f0ac17c04a8c9029444ba9fced0501c6bd75415445c825900efca7bb92c
+
+echo "Deployed migrator service"
