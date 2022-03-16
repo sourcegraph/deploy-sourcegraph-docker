306aa52b5e6ff8ec994c191df882cb4c8202dbd2
Crystal Augustus <crystal.augustus@sourcegraph.com>
2022-03-16 08:07:54 -0400

    test

--- .buildkite/validate-docker-compose.sh
+++ .buildkite/validate-docker-compose.sh
@@ -3,5 +3,6 @@
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 set -euxo pipefail
 
-cd docker-compose 
-docker-compose -f docker-compose.yaml config -q
+which docker-compose
+#cd docker-compose
+#docker-compose -f docker-compose.yaml config -q
