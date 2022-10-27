18a96078ba2ec39108d7857a75f44e8a68ad9520
Dave Try <davetry@gmail.com>
2022-10-27 16:04:35 -0500

    increase timeout, volume permissions (#870)

--- test/volume-config.sh
+++ test/volume-config.sh
@@ -2,6 +2,14 @@
 
 # Create volume directories.
 cd /deploy-sourcegraph-docker
+echo
+echo "creating deployment for volume directories"
+echo
+./pure-docker/deploy.sh
+echo
+echo "tearing down deployment for volume directories"
+echo
+./pure-docker/teardown.sh
 # Set permissions on volume directories.
 #
 # IMPORTANT: If these change, or a new service is introduced, it must be explicitly called out in
@@ -13,6 +21,7 @@ pushd ~/sourcegraph-docker
 chown -R 100:101 gitserver* prometheus-v2* worker* repo-updater* searcher* sourcegraph-frontend* symbols* zoekt* minio-disk
 chown -R 999:1000 redis-store-disk redis-cache-disk
 chown -R 472:472 grafana-disk
-chown -R 999:999 pgsql-disk codeintel-db-disk codeinsights-db-disk
+chown -R 999:999 pgsql-disk codeintel-db-disk
+chown -R 70:70 codeinsights-db-disk
 popd
 echo "Ready to deploy"
