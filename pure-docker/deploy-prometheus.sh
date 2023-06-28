7a48c24bce32000be758c99e4b2ff0edc6766692
Jean-Hadrien Chabran <jh@chabran.fr>
2023-06-28 13:04:58 +0200

    test with new prom image

--- pure-docker/deploy-prometheus.sh
+++ pure-docker/deploy-prometheus.sh
@@ -21,8 +21,7 @@ docker run --detach \
     -v $VOLUME:/prometheus \
     -v $(pwd)/../prometheus:/sg_prometheus_add_ons \
     -e SRC_FRONTEND_INTERNAL=sourcegraph-frontend-internal:3090 \
-    -e GODEBUG=netdns=go \
-    index.docker.io/sourcegraph/prometheus:5.1.0@sha256:6f2848c3aad6d6f8b82be138bb581e07129213624264fc0b20ab96b3c8874438
+    us.gcr.io/sourcegraph-dev/prometheus@sha256:33908e4d62102f4fb488f910861a4710c6426e7c95e2cb4b0d8c5b43a12e5abd
 
 # index.docker.io/sourcegraph/prometheus:5.0.6@sha256:08fd31f6ba5289ac9468c2e456d4cad6076889195f4c23722273ec1d80a2f317
 
