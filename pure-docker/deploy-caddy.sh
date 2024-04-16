4a0ce1818950d6f5caf71ab32b5d5338069a440c
Will Dollman <will.dollman@sourcegraph.com>
2024-04-16 10:52:09 +0100

    Replace upstream caddy with internal pinned version

--- pure-docker/deploy-caddy.sh
+++ pure-docker/deploy-caddy.sh
@@ -32,4 +32,4 @@ docker run --detach \
   -p 0.0.0.0:443:443 \
   -v "$VOLUME:/caddy-storage" \
   --mount type=bind,source="$(pwd)"/../caddy/builtins/http.Caddyfile,target=/etc/caddy/Caddyfile \
-  index.docker.io/caddy:2.7.6-alpine@sha256:80ca561981768b2c3568cc4bef3d4cd1f11c2a625c806bedeb8453aef98779a0
+  index.docker.io/sourcegraph/caddy:269106_2024-04-15_5.3-d8deee19f4ae@sha256:13b9ee03ca9feba90d83129922f96ee9c1b40064a048b73feac3a334e27c3aa1
