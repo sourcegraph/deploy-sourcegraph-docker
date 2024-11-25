f3e26648704db462607c6353fb0ab689ac486183
Anish Lakhwara <anish+github@lakhwara.com>
2024-11-25 14:22:54 -0800

    fix: use postgresql 16 images (#1116)
    We need to use the new postgresql images
    
    ### Checklist
    
    <!--
    Kubernetes and Docker Compose MUST be kept in sync. You should not merge
    a change here
    without a corresponding change in the other repository, unless it truly
    is specific to
    this repository. If uneeded, add link or explanation of why it is not
    needed here.
    -->
    * [x] Sister
    [deploy-sourcegraph](https://github.com/sourcegraph/deploy-sourcegraph)
    change:
    * [x] Sister
    [customer-replica](https://github.com/sourcegraph/deploy-sourcegraph-docker-customer-replica-1)
    change (if necessary, for any changes affecting pure-docker or
    configuration):
    * [x] All images have a valid tag and SHA256 sum
    ### Test plan
    CI
    <!--
    As part of SOC2/GN-104 and SOC2/GN-105 requirements, all pull requests
    are REQUIRED to
    provide a "test plan". A test plan is a loose explanation of what you
    have done or
    implemented to test this, as outlined in our Testing principles and
    guidelines:
    
    https://docs.sourcegraph.com/dev/background-information/testing_principles
      Write your test plan here after the "Test plan" header.
    -->

--- pure-docker/deploy-codeinsights-db.sh
+++ pure-docker/deploy-codeinsights-db.sh
@@ -13,17 +13,17 @@ VOLUME="$HOME/sourcegraph-docker/codeinsights-db-disk"
 ./ensure-volume.sh $VOLUME 999
 
 docker run --detach \
-    --name=codeinsights-db \
-    --network=sourcegraph \
-    --restart=always \
-    --cpus=4 \
-    --memory=2g \
-    -e POSTGRES_DB=postgres \
-    -e POSTGRES_PASSWORD=password \
-    -e POSTGRES_USER=postgres \
-    -e PGDATA=/var/lib/postgresql/data/pgdata \
-    -v $VOLUME:/var/lib/postgresql/data/ \
-    index.docker.io/sourcegraph/codeinsights-db:187572_2022-12-06_cbecc5321c7d@sha256:4dd89a1279e7d55ddcb5b570d0bb544422dd1ecb0e3662f23d22f9643837c2b5
+  --name=codeinsights-db \
+  --network=sourcegraph \
+  --restart=always \
+  --cpus=4 \
+  --memory=2g \
+  -e POSTGRES_DB=postgres \
+  -e POSTGRES_PASSWORD=password \
+  -e POSTGRES_USER=postgres \
+  -e PGDATA=/var/lib/postgresql/data/pgdata \
+  -v $VOLUME:/var/lib/postgresql/data/ \
+  index.docker.io/sourcegraph/postgresql-16-codeinsights:insiders
 
 # Sourcegraph requires PostgreSQL 12+. Generally newer versions are better,
 # but anything 12 and higher is supported.
